abaa118ac5d4efe196a9ff52ef1774a33bc1faf8
anhkhoivu <anhkhoivu95@gmail.com>
2016-09-25 22:56:24 -0700

    Some changes

---
+++ CECS 475 - Lab Assignment 05/CECS 475 - Lab Assignment 05/Model/SalariedEmployee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CECS_475___Lab_Assignment_05
+{
+    class SalariedEmployee : Employee, INotifyPropertyChanged
+    {
+        private decimal paymentAmount;
+
+        /// <summary>
+        /// four-parameter constructor 
+        /// </summary>
+        /// <param name="first">Parameter that takes the first name of an employee.</param>
+        /// <param name="last">Parameter that takes the last name of an employee</param>
+        /// <param name="ssn">Parameter that takes the SNN of an Employee.</param>
+        /// <param name="salary">Parameter that takes in the salary of an Employee.</param>
+        public SalariedEmployee(string first, string last, string ssn,
+           decimal salary) : base(first, last, ssn)
+        {
+            GetPaymentAmount = salary; // validate salary via property
+        } // end four-parameter SalariedEmployee constructor
+
+        /// <summary>
+        /// property that gets and sets salaried employee's salary
+        /// </summary>
+        public decimal GetPaymentAmount
+        {
+            get
+            {
+                return paymentAmount;
+            } // end get
+            set
+            {
+                if (value >= 0) // validation
+                    paymentAmount = value;
+                else
+                    throw new ArgumentOutOfRangeException("WeeklySalary",
+                       value, "WeeklySalary must be >= 0");
+            } // end set
+        } // end property WeeklySalary
+
+        public decimal Salary
+        {
+            get
+            {
+                return Earnings();
+            }
+            set
+            {
+                OnPropertyChanged("Salary");
+            }
+        }
+
+        /// <summary>
+        /// This method returns the amount that an Emyploee earns.
+        /// </summary>
+        /// <returns>returns the amount that an Employee earned.</returns>
+        public override decimal Earnings()
+        {
+            return paymentAmount;
+        }
+    }
+}
---
+++ CECS 475 - Lab Assignment 05/CECS 475 - Lab Assignment 05/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CECS_475___Lab_Assignment_05.ViewModel
+{
+    class EmployeeViewModel
+    { 
+        private IPayable[] payableObjects;
+
+        public EmployeeViewModel()
+        {
+            payableObjects = new IPayable[8];
+            payableObjects[0] = new SalariedEmployee("John", "Smith", "111-11-1111", 700M);
+            payableObjects[1] = new SalariedEmployee("Antonio", "Smith", "555-55-5555", 800M);
+            payableObjects[2] = new SalariedEmployee("Victor", "Smith", "444-44-4444", 600M);
+            payableObjects[3] = new HourlyEmployee("Karen", "Price", "222-22-2222", 16.75M, 40M);
+            payableObjects[4] = new HourlyEmployee("Ruben", "Zamora", "666-66-6666", 20.00M, 40M);
+            payableObjects[5] = new CommissionEmployee("Sue", "Jones", "333-33-3333", 10000M, .06M);
+            payableObjects[6] = new BasePlusCommissionEmployee("Bob", "Lewis", "777-77-7777", 5000M, .04M, 300M);
+            payableObjects[7] = new BasePlusCommissionEmployee("Lee", "Duarte", "888-88-888", 5000M, .04M, 300M);
+        }
+
+        public IPayable[] PayableObjects
+        {
+            get
+                { return payableObjects; }
+        }
+
+        private ICommand mUpdater;
+        private ICommand updateLastName;
+        private ICommand restore;
+
+        public ICommand UpdateCommand
+        {
+            get
+            {
+                if (mUpdater == null)
+                    mUpdater = new Updater();
+                return mUpdater;
+            }
+            set
+            {
+                mUpdater = value;
+            }
+        }
+
+        public ICommand SortByLastName
+        {
+            get
+            {
+                if (updateLastName == null)
+                    updateLastName = new sortByLastName(new EmployeeViewModel());
+                return updateLastName;
+            }
+            set
+            {
+                Array.Sort(payableObjects);
+            }
+        }
+
+        public ICommand restoreCommand
+        {
+            get
+            {
+                if (restore == null)
+                    restore = new restoreOriginal();
+                return restore;
+            }
+            set
+            {
+                new EmployeeViewModel();
+            }
+        }
+
+        private class Updater : ICommand
+        {
+            #region ICommand Members
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+            public event EventHandler CanExecuteChanged;
+
+            public void Execute(object parameter)
+            {
+            }
+            #endregion
+        }
+
+        private class sortByLastName : ICommand
+        {
+            EmployeeViewModel evm;
+
+            public sortByLastName(EmployeeViewModel newEVM)
+            {
+                evm = newEVM;
+            }
+
+            #region ICommand Members
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public void Execute(object parameter)
+            {
+               
+            }
+            #endregion
+        }
+
+        private class restoreOriginal : ICommand
+        {
+            EmployeeViewModel evm = new EmployeeViewModel();
+
+            public restoreOriginal()
+            {
+                
+            }
+
+            #region ICommand Members
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+            public event EventHandler CanExecuteChanged;
+
+            public void Execute(object parameter)
+            {
+
+            }
+            #endregion
+        }
+    }
+}
