c240f17c8d840aec5dc2cd99bd1c5da637a5adfb
anhkhoivu <anhkhoivu95@gmail.com>
2016-10-11 23:40:41 -0700

    Changed some stuff in Lab 6

--- CECS 475 - Lab Assignment 06 - Part A/CECS 475 - Lab Assignment 06 - Part A/View/MainWindow.xaml.cs
+++ CECS 475 - Lab Assignment 06 - Part A/CECS 475 - Lab Assignment 06 - Part A/View/MainWindow.xaml.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Data;
+using System.ComponentModel;
 
 namespace CECS_475___Lab_Assignment_06___Part_A
 {
@@ -21,10 +14,19 @@ namespace CECS_475___Lab_Assignment_06___Part_A
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Default constructor for MainWindow
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Function that triggers when combobox selection has changed
+        /// </summary>
+        /// <param name="sender">default parameter that takes in the changed object</param>
+        /// <param name="e">parameter that was triggered by selection change.</param>
         private void selection_Changed(object sender, RoutedEventArgs e)
         {
             if (cmb.SelectedItem == i1)
@@ -38,8 +40,7 @@ namespace CECS_475___Lab_Assignment_06___Part_A
         }
     }
 
-    // Requires using System.Collections.ObjectModel;
-    public class List1 : ObservableCollection<Employee>
+    public class EmployeeRoster : ObservableCollection<Employee>
     {
         // Creating the Tasks collection in this way enables data binding from XAML.
     }
