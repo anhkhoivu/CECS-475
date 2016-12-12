50d978e4b03eb5fb635988b14cc64b91d6ad268c
anhkhoivu <anhkhoivu95@gmail.com>
2016-12-12 10:17:00 -0800

    Updating repository with completed assignments

---
+++ CECS 475 - Lab Assignment 08/475 Lab 4 Part 3/dbo.Student.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Student] (
+    [StudentID]   INT          IDENTITY (1, 1) NOT NULL,
+    [StudentName] VARCHAR (50) NULL,
+    [StandardId]  INT          NULL,
+    [RowVersion]  ROWVERSION   NOT NULL,
+    CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED ([StudentID] ASC),
+    CONSTRAINT [FK_Student_Standard] FOREIGN KEY ([StandardId]) REFERENCES [dbo].[Standard] ([StandardId]) ON DELETE CASCADE
+
+);
+GO
+UPDATE [dbo].[Student]
+SET StudentID = Student.StudentID
+WHERE StudentID = StudentID;
+GO
+
+ALTER TABLE [dbo].[Student] NOCHECK CONSTRAINT [FK_Student_Standard]
+GO
